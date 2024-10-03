822256413162ecb3c57bd5f97e78c5c2f03cd68c
javycito <enjherjavier01@gmail.com>
2024-10-02 21:45:34 -0400

    Tarea Semana 3 (primera versión)

--- 1raTarea/Program.cs
+++ 1raTarea/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using _1raTarea;
+using _1raTarea.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
